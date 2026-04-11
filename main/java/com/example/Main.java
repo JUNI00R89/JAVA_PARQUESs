6c04905c2ec6bb2eb09f1b0467389e48a7ac041a
frank <frankjunior5400@gmail.com>
2026-04-10 19:05:58 -0500

    Proyecto: Sistema de Parques de Medellinn

--- main/java/com/example/Main.java
+++ main/java/com/example/Main.java
@@ -2,17 +2,17 @@ package com.example;
 
 import java.util.ArrayList;
 
-import com.MuseoDeAntioquia;
+
 
 public class Main {
     public static void main(String[] args) {
         ArrayList<EspacioCultural> lugares = new ArrayList<>();
 
         lugares.add(new JardinBotanico());
-        lugares.add(new MuseoDeAntioquia());
-        lugares.add(new ParqueExplora());
+        lugares.add((EspacioCultural) new MuseoDeAntioquia());
+        lugares.add((EspacioCultural) new ParqueExplora());
 
-        String[] nombres = {"Jardín Botánicooo", "Museo de Antioquia", "Parque Explora"};
+        String[] nombres = {"Jardín Botánicooo", "Museo de Antioquiaaa", "Parque Explora"};
 
         for (int i = 0; i < lugares.size(); i++) {
             EspacioCultural lugar = lugares.get(i);
